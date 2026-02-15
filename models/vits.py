b57c8a99d1654e0f280c9da52eed7776727df520
solhee-hwang <solhee0912@kookmin.ac.kr>
2026-02-16 02:11:19 +0900

    fix : import error

--- models/vits.py
+++ models/vits.py
@@ -11,8 +11,13 @@ from functools import partial, reduce
 from operator import mul
 
 from timm.models.vision_transformer import VisionTransformer, _cfg
-from timm.models.layers.helpers import to_2tuple
-from timm.models.layers import PatchEmbed
+try:
+    # timm >= 1.0
+    from timm.layers import PatchEmbed, to_2tuple
+except ImportError:
+    # timm < 1.0
+    from timm.models.layers import PatchEmbed
+    from timm.models.layers.helpers import to_2tuple
 
 __all__ = [
     'vit_small', 
